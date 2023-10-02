44f297e8f159883cf55a55826d69de3496991346
HammadZuberi <hammad.zuberi@hrsgbpo.com>
2023-10-02 15:33:20 -0400

    register form and model state validation

--- ClientAPP/src/app/account/register/register.component.ts
+++ ClientAPP/src/app/account/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AccountService } from '../account.service';
+import { Router } from '@angular/router';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +9,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+  complexPassword: string | RegExp ="(?=^.{6,10}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\\s).*$";
+errors :string[] |null=null;  
+  constructor(private fb: FormBuilder,
+    private accountService :AccountService, private router:Router ){
 
+  }
+
+  registerForm= this.fb.group({
+
+    displayName: ['',Validators.required],
+    email:['',Validators.required,Validators.email],
+    password:['',Validators.required,Validators.pattern(this.complexPassword)]
+  })
+
+  onSubmit(){
+    this.accountService.register(this.registerForm.value).subscribe({
+      next:() => this.router.navigateByUrl('/shop'),
+      error: error=> this.errors=error.errors
+    })
+  }
 }
