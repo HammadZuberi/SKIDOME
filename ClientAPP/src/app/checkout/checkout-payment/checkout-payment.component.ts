734ed85d69617eca28a5b0ea24a7a9ccccad50c6
HammadZuberi <hammad.zuberi@hrsgbpo.com>
2024-05-23 23:57:07 -0400

    adding stripe.js Components

--- ClientAPP/src/app/checkout/checkout-payment/checkout-payment.component.ts
+++ ClientAPP/src/app/checkout/checkout-payment/checkout-payment.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { BasketService } from 'src/app/basket/basket.service';
 import { CheckoutService } from '../checkout.service';
@@ -6,21 +6,69 @@ import { ToastrService } from 'ngx-toastr';
 import { Basket } from 'src/app/shared/Models/Basket';
 import { Address } from 'src/app/shared/Models/User';
 import { NavigationExtras, Router } from '@angular/router';
+import {
+  Stripe,
+  StripeCardCvcElement,
+  StripeCardExpiryElement,
+  StripeCardNumberElement,
+  loadStripe,
+} from '@stripe/stripe-js';
 
 @Component({
   selector: 'app-checkout-payment',
   templateUrl: './checkout-payment.component.html',
   styleUrls: ['./checkout-payment.component.scss'],
 })
-export class CheckoutPaymentComponent {
+export class CheckoutPaymentComponent implements OnInit {
   @Input() checkoutForm?: FormGroup;
+  @ViewChild('cardNumber') cardNumberElement?: ElementRef;
+  @ViewChild('cardExpiry') cardExpiryElement?: ElementRef;
+  @ViewChild('cardCvc') cardCvcElement?: ElementRef;
+  stripe: Stripe | null = null;
+  //class property
+  cardExpiry?: StripeCardExpiryElement;
+  cardCvc?: StripeCardCvcElement;
+  cardNumber?: StripeCardNumberElement;
+  cardErrors: any;
 
   constructor(
     private basketService: BasketService,
     private checkoutService: CheckoutService,
     private toastr: ToastrService,
-    private router:Router
+    private router: Router
   ) {}
+
+  //laod stripe
+  ngOnInit(): void {
+//publish key
+    loadStripe('pk_test_51OcIyAChqXGeDg7By9DTylUDHQ6SPzHpooYhKvla015yKHqzfwSPxzE7g0vln19pmWDZLHFiQL87W4sRxx1qdWk300N6ngbhte').then(stripe=> {
+      this.stripe =stripe;
+      const elements = stripe?. elements();
+      if(elements){
+        this.cardNumber = elements.create('cardNumber');
+        this.cardNumber.mount(this.cardNumberElement?.nativeElement);
+        this.cardNumber.on('change',event=>{
+          if(event.error) this.cardErrors = event.error.message;
+          else this.cardErrors= null;
+        });
+
+        
+        this.cardExpiry = elements.create('cardExpiry');
+        this.cardExpiry.mount(this.cardExpiryElement?.nativeElement); 
+        this.cardExpiry.on('change',event=>{
+          if(event.error) this.cardErrors = event.error.message;
+          else this.cardErrors= null;
+        });
+        
+        this.cardCvc = elements.create('cardCvc');
+        this.cardCvc.mount(this.cardCvcElement?.nativeElement); 
+        this.cardCvc.on('change',event=>{
+          if(event.error) this.cardErrors = event.error.message;
+          else this.cardErrors= null;
+        });
+      }
+    });
+  }
 
   submitOrder() {
     const basket = this.basketService.getCurrentBasketValue();
@@ -37,14 +85,12 @@ export class CheckoutPaymentComponent {
         this.basketService.deleteLocalBasket();
         //delete form local storage application storage,
 
-        
         console.log(order);
-        const NavigationExtras:NavigationExtras={state:order};
-        this.router.navigate(['checkout/success'],NavigationExtras);
+        const NavigationExtras: NavigationExtras = { state: order };
+        this.router.navigate(['checkout/success'], NavigationExtras);
       },
     });
   }
-
 
   private getOrderToCreate(basket: Basket) {
     const DeliveryMethodId = this.checkoutForm
--- ClientAPP/src/app/checkout/checkout-payment/dist/checkout-payment.component.js
+++ ClientAPP/src/app/checkout/checkout-payment/dist/checkout-payment.component.js
@@ -8,13 +8,51 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 exports.__esModule = true;
 exports.CheckoutPaymentComponent = void 0;
 var core_1 = require("@angular/core");
+var stripe_js_1 = require("@stripe/stripe-js");
 var CheckoutPaymentComponent = /** @class */ (function () {
     function CheckoutPaymentComponent(basketService, checkoutService, toastr, router) {
         this.basketService = basketService;
         this.checkoutService = checkoutService;
         this.toastr = toastr;
         this.router = router;
+        this.stripe = null;
     }
+    //laod stripe
+    CheckoutPaymentComponent.prototype.ngOnInit = function () {
+        var _this = this;
+        //publish key
+        stripe_js_1.loadStripe('pk_test_51OcIyAChqXGeDg7By9DTylUDHQ6SPzHpooYhKvla015yKHqzfwSPxzE7g0vln19pmWDZLHFiQL87W4sRxx1qdWk300N6ngbhte').then(function (stripe) {
+            var _a, _b, _c;
+            _this.stripe = stripe;
+            var elements = stripe === null || stripe === void 0 ? void 0 : stripe.elements();
+            if (elements) {
+                _this.cardNumber = elements.create('cardNumber');
+                _this.cardNumber.mount((_a = _this.cardNumberElement) === null || _a === void 0 ? void 0 : _a.nativeElement);
+                _this.cardNumber.on('change', function (event) {
+                    if (event.error)
+                        _this.cardErrors = event.error.message;
+                    else
+                        _this.cardErrors = null;
+                });
+                _this.cardExpiry = elements.create('cardExpiry');
+                _this.cardExpiry.mount((_b = _this.cardExpiryElement) === null || _b === void 0 ? void 0 : _b.nativeElement);
+                _this.cardExpiry.on('change', function (event) {
+                    if (event.error)
+                        _this.cardErrors = event.error.message;
+                    else
+                        _this.cardErrors = null;
+                });
+                _this.cardCvc = elements.create('cardCvc');
+                _this.cardCvc.mount((_c = _this.cardCvcElement) === null || _c === void 0 ? void 0 : _c.nativeElement);
+                _this.cardCvc.on('change', function (event) {
+                    if (event.error)
+                        _this.cardErrors = event.error.message;
+                    else
+                        _this.cardErrors = null;
+                });
+            }
+        });
+    };
     CheckoutPaymentComponent.prototype.submitOrder = function () {
         var _this = this;
         var basket = this.basketService.getCurrentBasketValue();
@@ -49,6 +87,15 @@ var CheckoutPaymentComponent = /** @class */ (function () {
     __decorate([
         core_1.Input()
     ], CheckoutPaymentComponent.prototype, "checkoutForm");
+    __decorate([
+        core_1.ViewChild('cardNumber')
+    ], CheckoutPaymentComponent.prototype, "cardNumberElement");
+    __decorate([
+        core_1.ViewChild('cardExpiry')
+    ], CheckoutPaymentComponent.prototype, "cardExpiryElement");
+    __decorate([
+        core_1.ViewChild('cardCvc')
+    ], CheckoutPaymentComponent.prototype, "cardCvcElement");
     CheckoutPaymentComponent = __decorate([
         core_1.Component({
             selector: 'app-checkout-payment',
